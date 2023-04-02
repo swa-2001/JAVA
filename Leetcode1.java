336192e8991d1c1f6bf76df0deb80dcdcc98a93c
swa-2001 <109035521+swa-2001@users.noreply.github.com>
2023-04-02 16:45:52 +0530

    Update Leetcode1.java

--- Leetcode1.java
+++ Leetcode1.java
@@ -12,12 +12,12 @@ public class Leetcode1 {
         }
 	}
 	public static void main(String[] args) {
-		 int arr[]= {1,2,3,4,5,6,7};                                 //1 , 2 , 3 , 4 , 5 , 6 , 7
+		 int arr[]= {1,2,3,4,5,6,7};                                                                             //   1 , 2 , 3 , 4 , 5 , 6 , 7
 		 int k=3;												//                   <---k------>   step1
-		 int len=arr.length;									//                    reverse
-		 Leetcode1 obj=new Leetcode1();							//    <--len-k----->                step2
-		 obj.reverse(arr,0,len-k-1);							//     reverse
-	       obj. reverse(arr,len-k,len-1);  						//             reverse whole array   step3
+		 int len=arr.length;									//                                      reverse
+		 Leetcode1 obj=new Leetcode1();							//                            <--len-k----->                step2
+		 obj.reverse(arr,0,len-k-1);							//                               reverse
+	       obj. reverse(arr,len-k,len-1);  						//                                       reverse whole array        step3
 	        obj.reverse(arr,0,len-1);
 	        for(int i=0;i<len;i++) {
 	        	System.out.print(arr[i]+" ");
