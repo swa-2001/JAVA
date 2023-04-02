674d59605316a2491c7c56df02a0fb90ca88eb80
swa-2001 <109035521+swa-2001@users.noreply.github.com>
2023-04-02 16:44:13 +0530

    Add files via upload

---
+++ Leetcode1.java
@@ -0,0 +1,27 @@
+//CONVERT STRING FROM UPPER TO LOWER CASE WITHOUT BUILT IN FUNCTIONS
+package practice;
+import java.util.*;
+public class Leetcode1 {
+	void reverse(int[] arr,int low,int high) {
+		while(low<high){
+            int temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+	}
+	public static void main(String[] args) {
+		 int arr[]= {1,2,3,4,5,6,7};                                 //1 , 2 , 3 , 4 , 5 , 6 , 7
+		 int k=3;												//                   <---k------>   step1
+		 int len=arr.length;									//                    reverse
+		 Leetcode1 obj=new Leetcode1();							//    <--len-k----->                step2
+		 obj.reverse(arr,0,len-k-1);							//     reverse
+	       obj. reverse(arr,len-k,len-1);  						//             reverse whole array   step3
+	        obj.reverse(arr,0,len-1);
+	        for(int i=0;i<len;i++) {
+	        	System.out.print(arr[i]+" ");
+	        }
+	}
+	}
+
